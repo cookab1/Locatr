4eaa7d08dfcfac8d0792c804546723c6832038b9
unknown <cookab1@CS-317-9K7KFQ1.appd.appstate.edu>
2017-11-09 10:03:21 -0500

    added first xml files

--- app/src/main/java/com/bignerdranch/android/speedometer/SpeedometerActivity.java
+++ app/src/main/java/com/bignerdranch/android/speedometer/SpeedometerActivity.java
@@ -8,6 +8,6 @@ public class SpeedometerActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_speedometer);
+        setContentView(R.layout.fragment_speedometer);
     }
 }
---
+++ app/src/main/java/com/bignerdranch/android/speedometer/SpeedometerFragment.java
@@ -0,0 +1,10 @@
+package com.bignerdranch.android.speedometer;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by cookab1 on 11/9/2017.
+ */
+
+public class SpeedometerFragment extends Fragment {
+}
