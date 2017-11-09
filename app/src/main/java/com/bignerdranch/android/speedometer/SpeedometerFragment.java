ec4f02233f1d8daab8ba9871dcd3a9284cd67adc
unknown <cookab1@CS-317-9K7KFQ1.appd.appstate.edu>
2017-11-09 10:48:12 -0500

    Now can connect to GoogleApiClient | page 633

--- app/src/main/java/com/bignerdranch/android/speedometer/SpeedometerFragment.java
+++ app/src/main/java/com/bignerdranch/android/speedometer/SpeedometerFragment.java
@@ -5,9 +5,13 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 
 /**
  * Created by cookab1 on 11/9/2017.
@@ -15,6 +19,7 @@ import android.widget.ImageView;
 
 public class SpeedometerFragment extends Fragment {
     private ImageView mImageView;
+    private GoogleApiClient mClient;
 
     public static SpeedometerFragment newInstance() {
         return new SpeedometerFragment();
@@ -24,20 +29,53 @@ public class SpeedometerFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setHasOptionsMenu(true);
+
+        mClient = new GoogleApiClient.Builder(getActivity())
+                .addApi(LocationServices.API)
+                .addConnectionCallbacks(new GoogleApiClient.ConnectionCallbacks() {
+                    @Override
+                    public void onConnected(Bundle bundle) {
+                        getActivity().invalidateOptionsMenu();
+                    }
+
+                    @Override
+                    public void onConnectionSuspended(int i) {
+
+                    }
+                })
+                .build();
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_speedometer, container, false);
 
-        mImageView = (ImageView) v.findViewById(R.id.image);
+        mImageView = v.findViewById(R.id.image);
 
         return v;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        getActivity().invalidateOptionsMenu();
+        mClient.connect();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        mClient.disconnect();
     }
 
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         super.onCreateOptionsMenu(menu, inflater);
         inflater.inflate(R.menu.fragment_speedometer, menu);
+
+        MenuItem searchItem = menu.findItem(R.id.action_locate);
+        searchItem.setEnabled(mClient.isConnected());
     }
 }
